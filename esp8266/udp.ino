391733ef9b5cc6c503286bb7ddcdba190b3b4c7e
solid <cristianosstec@gmail.com>
2021-05-06 21:58:09 -0300

    send json data

--- esp8266/udp.ino
+++ esp8266/udp.ino
@@ -1,5 +1,37 @@
 #include <ESP8266WiFi.h>
 #include <WiFiUdp.h>
+#include <NTPClient.h>
+#include <stdint.h>
+#include <ArduinoJson.h>
+#include "time.h"
+
+#define PROTOCOL_PAYLOAD_SIZE   1024
+#define DEVICE_NAME             16
+
+typedef enum 
+{
+    Temperature,
+    Barometer,
+    Air_Quality,
+    Rain_Detector,
+    Luminosity
+} Sensors;
+
+typedef struct 
+{
+    uint8_t id;
+    Sensors sensor;
+    char device[DEVICE_NAME];
+    time_t data;
+    uint32_t size;
+} Header;
+
+typedef struct 
+{
+    Header header;
+    char payload[PROTOCOL_PAYLOAD_SIZE];
+} Protocol;
+
   
 // Set WiFi credentials
 #define WIFI_SSID "SSID"
@@ -9,8 +41,15 @@
 // UDP
 WiFiUDP UDP;
 char packet[255];
-char reply[] = "Packet received!";
-  
+
+NTPClient timeClient(UDP, "pool.ntp.org");
+
+static char buffer[PROTOCOL_PAYLOAD_SIZE + sizeof(Header) + 1];
+static Protocol protocol;
+
+StaticJsonDocument<200> doc;
+
+ 
 void setup() {
   // Setup serial port
   Serial.begin(115200);
@@ -33,35 +72,22 @@ void setup() {
   Serial.println();
   Serial.print("Connected! IP address: ");
   Serial.println(WiFi.localIP());
- 
-  // Begin listening to UDP port
-//  UDP.begin(UDP_PORT);
-//  Serial.print("Listening on UDP port ");
-//  Serial.println(UDP_PORT);
   
 }
  
 void loop() {
  
-  // If packet received...
-//  int packetSize = UDP.parsePacket();
-//  if (packetSize) {
-//    Serial.print("Received packet! Size: ");
-//    Serial.println(packetSize); 
-//    int len = UDP.read(packet, 255);
-//    if (len > 0)
-//    {
-//      packet[len] = '\0';
-//    }
-//    Serial.print("Packet received: ");
-//    Serial.println(packet);
- 
-    // Send return packet
+    doc["id"] = 1;
+    doc["sensor"] = Temperature;
+    doc["device"] = "esp8266";
+     doc["time"] = timeClient.getEpochTime();
+    doc["size"] = strlen("Hello World");
+    doc["payload"] = "Hello World";
+
+    serializeJson(doc, buffer);
+    
     UDP.beginPacket("192.168.0.140", 1234);
-    UDP.write("esp8266");
+    UDP.write(buffer);
     UDP.endPacket();
-    delay(1000);
- 
-//  }
- 
+    delay(1000); 
 }
